6475544e854a8439521ae6f50e58f9ccaa845468
Omael N'guessan <omaelberiz@outlook.fr>
2023-07-10 14:41:49 +0000

    add authentification

---
+++ apps/auth/src/users/user.repository.ts
@@ -0,0 +1,15 @@
+import { AbstractRepository } from '@app/common';
+import { Injectable } from '@nestjs/common';
+import { UserDocument } from './model/user.schema';
+import { Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UserRepository extends AbstractRepository<UserDocument> {
+  protected readonly logger = new Logger(UserRepository.name);
+
+  constructor(@InjectModel(UserDocument.name) userModel: Model<UserDocument>) {
+    super(userModel);
+  }
+}
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '../config/config.module';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
+      imports: [],
       useFactory: (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
       }),
